7e1b1db4a2df88daa44ff737dc6d831c9753fb34
Vincent Dubois <dubois.vct@free.fr>
2016-11-04 09:10:59 +0100

    Config server support

--- src/main/java/io/github/vdubois/SpringCloudSampleMicroserviceApplication.java
+++ src/main/java/io/github/vdubois/SpringCloudSampleMicroserviceApplication.java
@@ -2,10 +2,12 @@ package io.github.vdubois;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringCloudSampleMicroserviceApplication {
 
 	public static void main(String[] args) {
