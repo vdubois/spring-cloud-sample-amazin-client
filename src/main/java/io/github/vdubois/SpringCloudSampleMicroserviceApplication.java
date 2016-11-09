ab32ccd83d2ddf841fdc8409c05cbf0b71c9aa6a
Vincent Dubois <dubois.vct@free.fr>
2016-11-09 21:25:40 +0100

    Adding Zipkin support

--- src/main/java/io/github/vdubois/SpringCloudSampleMicroserviceApplication.java
+++ src/main/java/io/github/vdubois/SpringCloudSampleMicroserviceApplication.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 public class SpringCloudSampleMicroserviceApplication {
+
+	@Bean
+	AlwaysSampler alwaysSampler() {
+		return new AlwaysSampler();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudSampleMicroserviceApplication.class, args);
