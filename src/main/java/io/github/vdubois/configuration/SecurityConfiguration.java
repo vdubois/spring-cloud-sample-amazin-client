7f9e66be26a1dcc57f06109c865858c5191a2ed3
Vincent Dubois <dubois.vct@free.fr>
2016-12-31 09:46:11 +0100

    [FEATURE] Adding JWT security

---
+++ src/main/java/io/github/vdubois/configuration/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package io.github.vdubois.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by vdubois on 30/12/16.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends JsonWebTokenSecurityConfiguration {
+
+    @Override
+    protected void setupAuthorization(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+
+                // allow anonymous access to /authenticate endpoint
+                .antMatchers("/authenticate").permitAll()
+
+                // allow anonymous to common static resources
+                .antMatchers(HttpMethod.GET, "/", "/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js")
+                .permitAll()
+
+                // authenticate all other requests
+                .anyRequest().authenticated();
+    }
+}
